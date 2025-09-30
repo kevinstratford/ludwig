055dbb5f1bae275871c552f9fdccdfac70e2d112
Kevin Stratford <kevin@cirrus.epcc.ed.ac.uk>
2025-09-30 08:50:09 +0100

    Add generic kernel

--- src/stats_free_energy.c
+++ src/stats_free_energy.c
@@ -4,12 +4,10 @@
  *
  *  Statistics for free energy density.
  *
- *  $Id$
- *
  *  Edinburgh Soft Matter and Statistical Physics Group and
  *  Edinburgh Parallel Computing Centre
  *
- *  (c) 2011-2018 The University of Edinburgh
+ *  (c) 2011-2025 The University of Edinburgh
  *
  *  Contributing authors:
  *  Kevin Stratford (kevin@epcc.ed.ac.uk)
@@ -21,6 +19,10 @@
 #include "control.h"
 #include "util.h"
 #include "stats_free_energy.h"
+
+int fe_stats_fluid_total(fe_t * fe, map_t * map, double sum[2]);
+__global__ void fe_stats_fluid_total_kernel(kernel_3d_t k3d, fe_t * fe,
+					    map_t * map, double * sum);
 
 /****************************************************************************
  *
@@ -41,13 +43,10 @@ int stats_free_energy_density(pe_t * pe, cs_t * cs, wall_t * wall, fe_t * fe,
 
 #define NSTAT 5
 
-  int ic, jc, kc, index;
   int ntstep;
   int nlocal[3];
-  int status;
   int ncolloid;
 
-  double fed;
   double fe_local[NSTAT];
   double fe_total[NSTAT];
   double rv;
@@ -73,19 +72,28 @@ int stats_free_energy_density(pe_t * pe, cs_t * cs, wall_t * wall, fe_t * fe,
   fe_local[3] = 0.0; /* surface free energy */
   fe_local[4] = 0.0; /* other wall free energy (walls only) */
 
-  for (ic = 1; ic <= nlocal[X]; ic++) {
-    for (jc = 1; jc <= nlocal[Y]; jc++) {
-      for (kc = 1; kc <= nlocal[Z]; kc++) {
+  if (0 == (fe->id == FE_ELECTRO || fe->id == FE_ELECTRO_SYMMETRIC)) {
+    fe_stats_fluid_total(fe, map, fe_local);
+  }
+  else {
+    /* We must fall back to the host serial version at the moment
+     * as there is no device implementation for the electrokinetics */
+    for (int ic = 1; ic <= nlocal[X]; ic++) {
+      for (int jc = 1; jc <= nlocal[Y]; jc++) {
+	for (int kc = 1; kc <= nlocal[Z]; kc++) {
 
-	index = cs_index(cs, ic, jc, kc);
-	map_status(map, index, &status);
+	  double fed = 0.0;
+	  int status = MAP_BOUNDARY;
+	  int index = cs_index(cs, ic, jc, kc);
+	  map_status(map, index, &status);
 
-	fe->func->fed(fe, index, &fed);
-	fe_local[0] += fed;
+	  fe->func->fed(fe, index, &fed);
+	  fe_local[0] += fed;
 
-	if (status == MAP_FLUID) {
+	  if (status == MAP_FLUID) {
 	    fe_local[1] += fed;
 	    fe_local[2] += 1.0;
+	  }
 	}
       }
     }
@@ -135,4 +143,113 @@ int stats_free_energy_density(pe_t * pe, cs_t * cs, wall_t * wall, fe_t * fe,
 #undef NSTAT
 
   return 0;
+}
+
+/*****************************************************************************
+ *
+ *  fe_stats_fluid_total
+ *
+ *  The components are: fe total, fluid volume
+ *
+ *****************************************************************************/
+
+int fe_stats_fluid_total(fe_t * fe, map_t * map, double sum[3]) {
+
+  const int nsz = 3*sizeof(double);
+  int nlocal[3] = {0};
+  double * dsum = NULL;
+  fe_t * target = NULL;
+
+  assert(fe);
+  assert(map);
+
+  cs_nlocal(map->cs, nlocal);
+
+  assert(fe->func->target);
+  fe->func->target(fe, &target);
+  assert(target);
+
+  tdpAssert( tdpMalloc((void ** ) &dsum, nsz) );
+  tdpAssert( tdpMemcpy(dsum, sum, nsz, tdpMemcpyHostToDevice) );
+
+  {
+    /* Kernel */
+    dim3 nblk = {};
+    dim3 ntpb = {};
+    cs_limits_t lim = {1, nlocal[X], 1, nlocal[Y], 1, nlocal[Z]};
+    kernel_3d_t k3d = kernel_3d(map->cs, lim);
+
+    kernel_3d_launch_param(k3d.kiterations, &nblk, &ntpb);
+    tdpLaunchKernel(fe_stats_fluid_total_kernel, nblk, ntpb, 0, 0,
+                    k3d, target, map->target, dsum);
+
+    tdpAssert( tdpPeekAtLastError() );
+    tdpAssert( tdpStreamSynchronize(0) );
+  }
+
+  tdpAssert( tdpMemcpy(sum, dsum, nsz, tdpMemcpyDeviceToHost) );
+  tdpAssert( tdpFree(dsum) );
+
+  return 0;
+}
+
+/*****************************************************************************
+ *
+ *  fe_stats_fluid_total_kernel
+ *
+ *****************************************************************************/
+
+__global__ void fe_stats_fluid_total_kernel(kernel_3d_t k3d, fe_t * fe,
+					    map_t * map, double * sum) {
+  int kindex = 0;
+  int tid = threadIdx.x;
+
+  __shared__ double tall[TARGET_PAD*TARGET_MAX_THREADS_PER_BLOCK];
+  __shared__ double tfed[TARGET_PAD*TARGET_MAX_THREADS_PER_BLOCK];
+  __shared__ double tvol[TARGET_PAD*TARGET_MAX_THREADS_PER_BLOCK];
+
+  tall[TARGET_PAD*tid] = 0.0;
+  tfed[TARGET_PAD*tid] = 0.0;
+  tvol[TARGET_PAD*tid] = 0.0;
+
+  for_simt_parallel(kindex, k3d.kiterations, 1) {
+
+    int ic = kernel_3d_ic(&k3d, kindex);
+    int jc = kernel_3d_jc(&k3d, kindex);
+    int kc = kernel_3d_kc(&k3d, kindex);
+    int index  = kernel_3d_cs_index(&k3d, ic, jc, kc);
+    int status = MAP_BOUNDARY;
+    double fed = 0.0;
+
+    map_status(map, index, &status);
+
+    fe->func->fed(fe, index, &fed);
+    tall[TARGET_PAD*tid] += fed;
+
+    if (status == MAP_FLUID) {
+      tfed[TARGET_PAD*tid] += fed;
+      tvol[TARGET_PAD*tid] += 1.0;
+    }
+  }
+
+  __syncthreads();
+
+  if (tid == 0) {
+    /* Accumulate block totals */
+    double ball = 0.0;
+    double bfed = 0.0;
+    double bvol = 0.0;
+
+    for (int it = 0; it < blockDim.x; it++) {
+      ball += tall[TARGET_PAD*it];
+      bfed += tfed[TARGET_PAD*it];
+      bvol += tvol[TARGET_PAD*it];
+    }
+
+    tdpAtomicAddDouble(sum + 0, ball);
+    tdpAtomicAddDouble(sum + 1, bfed);
+    tdpAtomicAddDouble(sum + 2, bvol);
+  }
+
+  return;
 }
